79c6db3544d0f2d5b99e58d474a0a5f45c764c4c
hardRPG <jan.weyrich@gmx.de>
2026-07-28 03:55:33 +0200

    Speichern der Analyse in seperater json Datei

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ psBaseCall = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed"
 
 params = {
     "key":PSAPI_KEY,
-    "url":testedURL
+    "url":testedURL,
+    "strategy":"MOBILE",
+    "locale":"de-DE"
 }
 
 def callPagespeedAPI():
     response = requests.get(psBaseCall,params=params)
     if response.ok:
-        print("Response:", response.json())
+        #print("Response:", response.json())
+        with open("analysen/pagespeed_response.json", "w", encoding="utf-8") as file:
+            json.dump(response.json(), file, ensure_ascii=False, indent=2)
     elif 400 <= response.status_code < 500:
         print("Client error:", response.status_code, response.json())
     else: 
